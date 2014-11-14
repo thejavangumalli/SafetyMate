452dafb24c8793ac0003a2c36425a2dc39014672
Theja Vangumalli <theja.vangumalli@gmail.com>
2014-11-14 10:56:24 -0800

    Initial commit

---
+++ mobile/src/main/java/com/safetymate/safetymate/MainActivity.java
@@ -0,0 +1,64 @@
+package com.safetymate.safetymate;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.RemoteInput;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.NotificationCompat.WearableExtender;
+
+import com.google.android.gms.wearable.WearableListenerService;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button button = (Button) findViewById(R.id.button_notify);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                int notificationId = 001;
+                // Create an intent for the reply action
+                Intent actionIntent = new Intent(getApplicationContext(), MainActivity.class);
+                PendingIntent actionPendingIntent =
+                        PendingIntent.getActivity(getApplicationContext(), 0, actionIntent,
+                                PendingIntent.FLAG_UPDATE_CURRENT);
+
+                // Create the action
+                NotificationCompat.Action action =
+                        new NotificationCompat.Action.Builder(R.drawable.danger,
+                                "Cancel", actionPendingIntent)
+                                .build();
+                long[] pattern = {0, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000};
+
+                // Build the notification and add the action via WearableExtender
+                Notification notification =
+                        new NotificationCompat.Builder(getApplicationContext())
+                                .setSmallIcon(R.drawable.danger)
+                                .setContentTitle("Safety Mate")
+                                .setContentText("Are you in Danger? Swipe right to stop sending notification")
+                                .extend(new WearableExtender().addAction(action))
+                                .setVibrate(pattern)
+                                .build();
+                // Get an instance of the NotificationManager service
+                NotificationManagerCompat notificationManager =
+                        NotificationManagerCompat.from(getApplicationContext());
+
+                // Issue the notification with notification manager.
+                notificationManager.notify(notificationId, notification);
+
+            }
+        });
+    }
+
+}
